d9ff92df3363f921fb1499301d5d9fff7eb01859
Kenny Drobnack <kennydrobnack@gmail.com>
2015-12-30 12:33:29 -0500

    Added in code to convert from Arabic to Roman numerals and back for a range of numbers

--- number_conversion.rb
+++ number_conversion.rb
@@ -2,9 +2,12 @@
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '.', 'lib'))
 require 'arabic_numeral'
+require 'roman_numeral'
 
 
 (1..2000).each do |i|
   num = ArabicNumeral.new(i)
+  rnum = RomanNumeral.new(num.to_roman_numeral)
   puts "Converting #{i} to roman numeral #{num.to_roman_numeral}"
+  puts "And back to Arabic: #{rnum.to_arabic_numeral}"
 end
