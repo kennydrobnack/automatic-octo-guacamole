579d09189116a59f48e35770ae9a3b1a3e1d734d
Kenny Drobnack <kennydrobnack@gmail.com>
2016-01-03 21:41:50 -0500

    Finished up refactoring the to_roman_numeral function to use helper classes, reorganized the code and got all the tests passing

--- lib/arabic_numeral.rb
+++ lib/arabic_numeral.rb
@@ -7,57 +7,57 @@ class ArabicNumeral
 		return ""
 	end
 
-	def convert_five
-		return "V"
-	end
-
-	def convert_fifty
-		return "L"
-	end
-
-	def convert_five_hundred
-		return "D"
-	end 
-
 	def convert_ones(number)
 		return "I" * number
 	end
 
-	def convert_tens(number)
-		return "X" * (number/10).floor
-	end
-
-	def convert_hundreds(number)
-		return "C" * (number/100).floor
-	end
-
-	def convert_thousands(number)
-		return "M" * (number/1000).floor
-	end 
-
 	def convert_four
 		return "IV"
+	end
+
+	def convert_five
+		return "V"
 	end
 
 	def convert_nine
 		return "IX"
 	end
 
+	def convert_tens(number)
+		return "X" * (number/10).floor
+	end
+
 	def convert_forty
 		return "XL"
+	end
+
+	def convert_fifty
+		return "L"
 	end
 
 	def convert_ninety
 		return "XC"
 	end
 
+	def convert_hundreds(number)
+		return "C" * (number/100).floor
+	end
+
 	def convert_four_hundred
 		return "CD"
 	end
 
+	def convert_five_hundred
+		return "D"
+	end 
+
 	def convert_nine_hundred
 		return "CM"
-	end		
+	end	
+
+	def convert_thousands(number)
+		return "M" * (number/1000).floor
+	end 
 
   def to_roman_numeral(number = nil)
     if number.nil?
@@ -66,45 +66,34 @@ class ArabicNumeral
     case number
       when 0 #Handle weird cases where we get to 0 - that means previous steps have done all conversions
         return ""
-      when 5 #The "5" symbols first. They cannot be repeated or subtracted
-        return convert_five
-      when 50
-        return convert_fifty
-      when 500
-        return convert_five_hundred
-      when 1..3 #Repeatable symbols
+      when 1..3
         return convert_ones(number)
-      when 10..39
-        mult = (number/10).floor
-        return convert_tens(number) + to_roman_numeral(number - 10 * mult)
-      when 100..399
-        mult = (number/100).floor
-        return convert_hundreds(number) + to_roman_numeral(number - 100 * mult)
-      when 1000..3999
-        mult = (number/1000).floor
-        return convert_thousands(number) + to_roman_numeral(number - 1000 * mult)
-      when 4 #Special cases for the minus ones
-        return "IV"
-      when 9
-        return "IX"
-      when 40..49
-        return "XL" + to_roman_numeral(number - 40)
-      when 90..99
-        return "XC" + to_roman_numeral(number - 90)
-      when 400..499
-        return "CD" + to_roman_numeral(number - 400)
-      when 900..999
-        return "CM" + to_roman_numeral(number - 900)
-      when 1001..1989 #Now break off the biggest numbers
-        return to_roman_numeral(1000) + to_roman_numeral(number - 1000)
-      when 500..899
-        return to_roman_numeral(500) + to_roman_numeral(number - 500)
-      when 51..99
-        return to_roman_numeral(50) + to_roman_numeral(number - 50)
-      when 10..30
-        return to_roman_numeral(10) + to_roman_numeral(number - 10)
-      when 5..8
-        return to_roman_numeral(5) + to_roman_numeral(number - 5)
+			when 4
+				return convert_four
+      when 5
+				return convert_five
+			when 5..8
+        return convert_five + convert_ones(number - 5)
+			when 9
+				return convert_nine
+			when 10..39
+				return convert_tens(number) + to_roman_numeral(number % 10)
+			when 40..49
+				return convert_forty + to_roman_numeral(number - 40)
+			when 50..89
+				return convert_fifty + to_roman_numeral(number - 50)
+			when 90..99
+				return convert_ninety + to_roman_numeral(number - 90)
+			when 100..399
+				return convert_hundreds(number) + to_roman_numeral(number % 100)
+			when 400..499
+				return convert_four_hundred + to_roman_numeral(number - 400)
+			when 500..899
+				return convert_five_hundred + to_roman_numeral(number - 500)
+			when 900..999
+				return convert_nine_hundred + to_roman_numeral(number - 900)
+			when 1000..3999
+				return convert_thousands(number) + to_roman_numeral(number % 1000)
     end
     #Error case - this way we known something is broke instead of letting it silently fail
     return "Unknown number #{number}"
--- number_conversion.rb
+++ number_conversion.rb
@@ -11,3 +11,10 @@ require 'roman_numeral'
   puts "Converting #{i} to roman numeral #{num.to_roman_numeral}"
   puts "And back to Arabic: #{rnum.to_arabic_numeral}"
 end
+
+negative = ArabicNumeral.new(-1)
+too_big = ArabicNumeral.new(4000)
+puts "Converting -1 to Roman Numeral: #{negative.to_roman_numeral}"
+puts "Converting 4000 to Roman Numeral: #{too_big.to_roman_numeral}"
+
+
